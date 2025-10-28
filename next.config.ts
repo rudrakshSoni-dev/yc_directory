2e6ba616e0642124595ffc8984e137551c6dda2c
rudrakshSoni-dev <mrrssoni12@gmail.com>
2025-10-28 22:26:23 +0530

    Fixed Build Error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
+import withBundleAnalyzer from "@next/bundle-analyzer";
+
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
 
 const nextConfig: NextConfig = {
+  // only define safe public vars here
   typescript: {
     ignoreBuildErrors: true,
   },
