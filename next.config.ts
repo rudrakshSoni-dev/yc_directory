2ed06ab8b6aa071a638ac0e4a300d94eb2bb7492
rudrakshSoni-dev <mrrssoni12@gmail.com>
2025-10-28 22:38:38 +0530

    Readded Sentry

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-// import { withSentryConfig } from "@sentry/nextjs";
+import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 import withBundleAnalyzer from "@next/bundle-analyzer";
 
@@ -35,26 +35,26 @@ export const nextConfig: NextConfig = {
   },
 };
 
-// export default withSentryConfig(nextConfig, {
-//   // For all available options, see:
-//   // https://github.com/getsentry/sentry-webpack-plugin#options
+export default withSentryConfig(nextConfig, {
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options
 
-//   org: "none-fqi",
-//   project: "javascript-nextjs",
+  org: "none-fqi",
+  project: "javascript-nextjs",
 
-//   // Only print logs for uploading source maps in CI
-//   silent: !process.env.CI,
+  // Only print logs for uploading source maps in CI
+  silent: !process.env.CI,
 
-//   // For all available options, see:
-//   // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+  // For all available options, see:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-//   // Upload a larger set of source maps for prettier stack traces (increases build time)
-//   widenClientFileUpload: true,
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: true,
 
-//   // Automatically annotate React components to show their full name in breadcrumbs and session replay
-//   reactComponentAnnotation: {
-//     enabled: true,
-//   },
+  // Automatically annotate React components to show their full name in breadcrumbs and session replay
+  reactComponentAnnotation: {
+    enabled: true,
+  },
 
   // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
   // This can increase your server load as well as your hosting bill.
@@ -73,4 +73,4 @@ export const nextConfig: NextConfig = {
 //   // https://docs.sentry.io/product/crons/
 //   // https://vercel.com/docs/cron-jobs
 //   automaticVercelMonitors: true,
-// });
+});
